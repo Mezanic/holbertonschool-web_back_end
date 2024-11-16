19cdb1b1c05ae7e30530ac747440fdefd29d462c
Mezanic <ezanic.mael@gmail.com>
2024-11-16 19:32:24 +0100

    Added file for project ES6 classes

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value === 'number') {
+      this._sqft = value;
+    } else {
+      throw new TypeError('Sqft must be a number');
+    }
+  }
+}
