b35fd9d36aed86c9fa4b146b15b31930f15ae1ca
Mezanic <ezanic.mael@gmail.com>
2024-11-26 12:02:17 +0100

    For good sanity i take this code from generous Mr Laffite

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,24 +1,16 @@
 /* eslint-disable */
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+/* eslint-disable */
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => {
-      results.map((result) => {
-        let value;
-        if (result.status === 'fulfilled') {
-          value = result.value;
-        } else {
-          value = result.reason;
-        }
-        return {
-          status: result.status,
-          value,
-        };
-      });
-    });
+    const signUpPromise = signUpUser(firstName, lastName);
+    const uploadPromise = uploadPhoto(fileName);
+    return Promise.allSettled([signUpPromise, uploadPromise])
+        .then((results) =>
+            results.map((result) => ({
+                status: result.status,
+                value: result.status === 'fulfilled' ? result.value : result.reason,
+            }))
+        );
 }
