905b78c46094d3212f03dbc402803252a694a8d9
Mezanic <ezanic.mael@gmail.com>
2024-11-26 11:49:08 +0100

    fix resolve case with a good handle of status and body

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,7 +1,7 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      resolve('status : 200 body : Succes');
+      resolve({status: 200, body: 'Succes'});
     } else {
       reject(new Error('The fake API is not working currently'));
     }
