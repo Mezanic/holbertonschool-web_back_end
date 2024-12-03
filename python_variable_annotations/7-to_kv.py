38798bd24acc5d90aa8028372c8168fad00e1fdf
Mezanic <ezanic.mael@gmail.com>
2024-12-03 16:34:26 +0100

    Change order for Union argument

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return tuple with string and float"""
     return (k, v * v)
