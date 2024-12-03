e15bf78202c881af32e468f75cd5523d6d17a659
Mezanic <ezanic.mael@gmail.com>
2024-12-03 16:36:02 +0100

    import List

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,9 +2,9 @@
 """This Module define the task 9"""
 
 
-from typing import Iterable, Sequence, Tuple
+from typing import Iterable, Sequence, Tuple, List
 
 
-def element_length(lst: Iterable[Sequence]) -> list[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Function  for task 9"""
     return [(i, len(i)) for i in lst]
