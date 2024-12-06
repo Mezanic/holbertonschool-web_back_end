ad5eb6868df61b6ba813c12d195f3e23bcd1a81e
Mezanic <ezanic.mael@gmail.com>
2024-12-06 15:41:20 +0100

    Match return for the checker

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -11,5 +11,5 @@ async def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
-    result_time = (end_time - start_time) / n
-    return result_time
+    total_time = end_time - start_time
+    return total_time / n
