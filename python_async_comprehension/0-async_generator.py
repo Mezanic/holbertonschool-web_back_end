d7a8b7cefd0ca5e3e27e866ebf7235f21459c3f1
Mezanic <ezanic.mael@gmail.com>
2024-12-09 15:29:41 +0100

    Pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,9 +5,9 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """function for yield random float number"""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-        
