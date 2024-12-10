ae2d1e94fcabe028a7819408a144f02144b36d0b
Mezanic <ezanic.mael@gmail.com>
2024-12-10 09:50:09 +0100

    Added documentation

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,12 +3,12 @@
 
 import asyncio
 import time
-from typing import List
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
+    """Measure the total runtime and return it"""
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for i in range(4)))
     end_time = time.time()
