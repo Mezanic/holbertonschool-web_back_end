1ff27040bb1857710b0b461e3864b655f1a7b1f6
Mezanic <ezanic.mael@gmail.com>
2025-01-24 16:52:43 +0100

    Eslinted

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,18 @@
-// This module prompts the user for their name and logs it to the console.
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
+// Use the process module for to allow interaction with console
+const process = require('process');
 
-const rl = readline.createInterface({ input, output });
+// Prompt the user to input text
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-rl.question('Welcome to Holberton School, what is your name?', (answer) => {
-  console.log(`Your name is: ${answer}`);
-
-  rl.close();
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
+// Get user input
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
   }
+});
+
+// Print a message upon closing the input stream
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
